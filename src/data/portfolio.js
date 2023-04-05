521225896207a6ca249be4823e5117c6c2e185b3
diptapanda <diptapanda7@gmail.com>
2023-04-05 18:07:50 +0530

    fixed portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,15 +1,15 @@
 export default [
   {
-    title: "ExamPortal",
+    title: "Exam portal",
     imgUrl: "https://i0.wp.com/www.freestudentprojects.com/wp-content/uploads/2013/04/Online-Examin-Portal.jpg?fit=1024%2C682&ssl=1",
     stack: ["Angular", "SpringBoot", "MySql"],
     link: "hhttps://github.com/wlommusic/exam-portal",
   },
   {
-    title: "Minimal portfolio",
-    imgUrl: "../../public/ss.png",
-    stack: ["React", "Vite", "Tailwind"],
-    link: "http://google.com",
+    title: "Bug tracker",
+    imgUrl: "../../public/bugs.png",
+    stack: ["Angular", "Typescript", "Bootstrap"],
+    link: "https://bugtrackerangular.netlify.app",
   },
   {
     title: "E-commerce Website",
